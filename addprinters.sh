b85ccd65812ed241834c8c560215654ef009a66a
Jonathan Zacsh <jzacsh@gmail.com>
2015-01-22 21:30:10 -0500

    nit: leave no files behind where execution occurs
    Make use of MKTEMP(1) to default to downloading files to temporary location, and
    only use original code's location if the user already has an existing file
    there.
    
    NOTE: this is not a clean commit - includes some safety changes to `download`
    function that I made use of.

--- addprinters.sh
+++ addprinters.sh
@@ -5,38 +5,44 @@ function check_exists {
 }
 
 function download {
-	URL=$1
+	local URL=$1
+	local FILE
+
 	FILE=`basename $1`
 	
 	if check_exists wget; then
-		wget "$URL"
+		wget -O "$FILE" "$URL"
 	elif check_exists curl; then
 		curl "$URL" -o "$FILE"
 	else
-		echo "Please install either wget or curl"
+		echo "Please install either wget or curl" >&2
+		exit 1
 	fi
+
+	echo -n "$FILE"
 }
 
-if [ ! -f printers.conf ]; then
+PRINTER_CONF='printers.conf'
+if [ ! -f "$PRINTER_CONF" ]; then
+	PRINTER_CONF="$(mktemp -t ninja-unix_XXXXXX_printer.conf)"
 	URL=https://raw.github.com/adicu/ninja-unix/master/printers.conf
 	echo "No printers.conf. Attempting to download."
-	download $URL
+	mv "$(download "$URL")" "$PRINTER_CONF" >/dev/null
 fi
 
 PPD="hp-laserjet-9050-$(uname).ppd"
-
 if [ ! -f $PPD ]; then
-	URL=https://raw.github.com/adicu/ninja-unix/master/$PPD
 	echo "No ppd file. Attempting to download."
-	download $URL
+	URL=https://raw.github.com/adicu/ninja-unix/master/$PPD
+	PPD="$(mktemp -t "XXXXXX_${PPD}")"
+	mv "$(download "$URL")" "$PPD" >/dev/null
 fi
 
 LPADMIN=`which lpadmin`
-
 if [ -z $LPADMIN ]; then
-	echo "Could not find the lpadmin program."
-	echo "Either you have not installed CUPS, or lpadmin is not on your PATH"
-	exit
+	echo "Could not find the lpadmin program." >&2
+	echo "Either you have not installed CUPS, or lpadmin is not on your PATH" >&2
+	exit 2
 fi
 
 add_ninja(){
@@ -55,12 +61,12 @@ echo "What is your UNI?"
 read uni
 
 if [ -z $1 ]; then
-	read_config < printers.conf
+	read_config < "$PRINTER_CONF"
 else
-	exists=`grep $1 printers.conf | head -n 1`
+	exists=`grep $1 "$PRINTER_CONF" | head -n 1`
 	if [ -z "$exists" ]; then
-		echo "Could not find printer matching this pattern"
-		exit
+		echo "Could not find printer matching this pattern" >&2
+		exit 3
 	fi
-	grep $1 printers.conf | read_config
+	grep $1 "$PRINTER_CONF" | read_config
 fi
